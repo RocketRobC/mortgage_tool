b4e061d0ff8146a2ba48d9a13fbf1e0b2167d422
RocketRobC <rob@robcornish.net>
2017-04-30 22:56:53 +1000

    removed js, added form submit and rendered the template for index from the calculate path

--- bin/calc.rb
+++ bin/calc.rb
@@ -8,16 +8,16 @@ class Calc < Sinatra::Base
     set :public_folder, File.join(File.dirname(__FILE__), '../public')
     set :views, File.join(File.dirname(__FILE__), '../views')
     set :bind, '0.0.0.0'
-    set :data, MortgageCalc::Dashboard.new
   end
 
   get '/' do
-    @data = settings.data
-    haml :index
+    @data = MortgageCalc::Dashboard.new
+    haml :index, locals: { data: @data }
   end
 
   post '/calculate' do
-    settings.data = MortgageCalc::Dashboard.new(params_data)
+    @data = MortgageCalc::Dashboard.new(params_data)
+    haml :index, locals: { data: @data }
   end
 
   def params_data
